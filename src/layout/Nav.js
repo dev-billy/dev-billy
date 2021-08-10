92cec4c15b4f5d025a8e5f2e6a61778c911656e1
Billy <billypatrick767@gmail.com>
2021-08-10 18:43:45 +0300

    added nav mobile with styles

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,10 +1,14 @@
 import navlist from "./data/navlist";
 import { Link } from "react-router-dom";
+import styles from "./layout.module.scss";
+
 function Nav(props) {
   const { location } = props;
   const NavList = navlist(location);
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <div className={styles.logo}></div>
+      <div className={styles.menu}></div>
       <ul>
         {NavList.map((item, index) => (
           <li onClick={item.onClick} key={index}>
