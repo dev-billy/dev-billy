cb198554f7cfe85f8e69c8e0143c1cc13d5b075c
Billy <billypatrick767@gmail.com>
2021-08-11 16:56:56 +0300

    fixed mobile nav

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import navlist from "./data/navlist";
 import { Link as LinkRouter } from "react-router-dom";
 
 import styles from "./layout.module.scss";
 
 function Nav(props) {
+  const [open, setOpen] = useState(false);
+  const handleMenuClick = () => {
+    setOpen(!open);
+  };
   const { location } = props;
   const NavList = navlist(location);
   return (
@@ -11,8 +16,27 @@ function Nav(props) {
       <LinkRouter to="/">
         <div className={styles.logo}></div>
       </LinkRouter>
-      <div className={styles.menu}></div>
-      <ul>
+      <div
+        className={open ? styles.menuClose : styles.menuOpen}
+        onClick={handleMenuClick}
+      ></div>
+      <ul
+        className={`${styles.menuMobile} ${open ? styles.show : styles.hide}`}
+      >
+        {NavList.map((item, index) => (
+          <li onClick={item.onClick} key={index}>
+            <LinkRouter
+              to={item.link}
+              className={`${item.cta ? styles.cta : ""} ${
+                item.active ? styles.activeLink : ""
+              }`}
+            >
+              {item.text}
+            </LinkRouter>
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.menuLarge}>
         {NavList.map((item, index) => (
           <li onClick={item.onClick} key={index}>
             <LinkRouter
