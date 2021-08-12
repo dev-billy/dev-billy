52ce1b6350cecbdd1c9d97f9e636797707537732
Billy <billypatrick767@gmail.com>
2021-08-12 15:10:38 +0300

    added support for emailjs

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,17 +1,34 @@
 import Layout from "../../layout";
 import styles from "../pagestyles/contact.module.scss";
+import emailjs from "emailjs-com";
+require("dotenv").config();
+
 function Contact() {
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => console.log(result),
+        (error) => console.log(error.text)
+      );
+  };
   return (
     <Layout>
       <h1>Contact Me</h1>
       <div className={styles.container}>
         <div className={styles.textContent}>
           <h2>Feel Free To Send Me A Message</h2>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={sendEmail}>
             <div className={styles.inputs}>
               <label htmlFor="email">Email</label>
               <input
-                name="email"
+                name="user_email"
                 type="email"
                 required
                 placeholder="mail@example.com"
@@ -25,7 +42,9 @@ function Contact() {
                 rows={5}
               ></textarea>
             </div>
-            <button className={styles.sendBtn}>Send</button>
+            <button className={styles.sendBtn} type="submit">
+              Send
+            </button>
           </form>
         </div>
         <div className={styles.image}></div>
