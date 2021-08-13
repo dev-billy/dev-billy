f80ac440040b73bbb07767b4e9e1fa51c7031fba
Billy <billypatrick767@gmail.com>
2021-08-13 17:26:37 +0300

    added loading states to send a message

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Layout from "../../layout";
 import styles from "../pagestyles/contact.module.scss";
 import emailjs from "emailjs-com";
 require("dotenv").config();
 
 function Contact() {
+  const [status, setStatus] = useState({
+    code: 0,
+    message: "",
+  });
+  const [formData, setFormData] = useState({
+    user_email: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   const sendEmail = (e) => {
     e.preventDefault();
+    setStatus({ ...status, message: "Sending Message", code: 100 });
     emailjs
       .sendForm(
         process.env.REACT_APP_SERVICE_ID,
@@ -14,15 +28,42 @@ function Contact() {
         process.env.REACT_APP_USER_ID
       )
       .then(
-        (result) => console.log(result),
-        (error) => console.log(error.text)
-      );
+        (result) =>
+          setStatus({ ...status, code: result.status, message: result.text }),
+        (error) =>
+          setStatus({ ...status, code: error.status, message: error.text })
+      )
+      .then(() => {
+        setFormData({
+          user_email: "",
+          message: "",
+        });
+      });
   };
   return (
     <Layout>
       <h1>Contact Me</h1>
       <div className={styles.container}>
         <div className={styles.textContent}>
+          <div className={status.code !== 0 ? styles.loadingState : styles.off}>
+            <button
+              className={styles.closeBtn}
+              onClick={() => setStatus({ ...status, code: 0 })}
+            >
+              Close
+            </button>
+            <p>
+              {status.code === 200
+                ? "Thank you for your Message"
+                : status.message}
+            </p>
+            <div
+              className={status.code === 200 ? styles.complete : styles.off}
+            ></div>
+            <div
+              className={status.code === 100 ? styles.loader : styles.off}
+            ></div>
+          </div>
           <h2>Feel Free To Send Me A Message</h2>
           <form className={styles.form} onSubmit={sendEmail}>
             <div className={styles.inputs}>
@@ -31,6 +72,9 @@ function Contact() {
                 name="user_email"
                 type="email"
                 required
+                onChange={handleChange}
+                value={formData.user_email}
+                disabled={status.code === 100}
                 placeholder="mail@example.com"
               />
             </div>
@@ -40,9 +84,16 @@ function Contact() {
                 name="message"
                 placeholder="Message"
                 rows={5}
+                onChange={handleChange}
+                value={formData.message}
+                disabled={status.code === 100}
               ></textarea>
             </div>
-            <button className={styles.sendBtn} type="submit">
+            <button
+              className={styles.sendBtn}
+              type="submit"
+              disabled={status.code === 100}
+            >
               Send
             </button>
           </form>
