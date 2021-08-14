91c82706d0d237a9229261117f2584bfb1984eca
Billy <billypatrick767@gmail.com>
2021-08-14 23:13:09 +0300

    added the basic layout for portfolio page

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,9 +1,52 @@
 import Layout from "../../layout";
+import styles from "../pagestyles/portfolio.module.scss";
 function Portfolio() {
   return (
     <Layout>
-      <h1>Portfolio</h1>
-      <p>In the works, updating will be availabe asap</p>
+      <section className={styles.intro}>
+        <h1>My Projects</h1>
+        <p>
+          These are some of the projects, I have built over the years with
+          interesting stories behind them
+        </p>
+      </section>
+
+      <section className={styles.projectList}>
+        <div className={styles.project}>
+          <div className={styles.highlightImg}></div>
+          <div className={styles.textContent}>
+            <div className={styles.titles}>
+              <h2>Project Title</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Et.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et.
+              </p>
+            </div>
+            <ul>
+              <li>Figma</li>
+              <li>Figma</li>
+            </ul>
+            <button className={styles.readMore}>Read More</button>
+          </div>
+        </div>
+        <div className={styles.project}>
+          <div className={styles.highlightImg}></div>
+          <div className={styles.textContent}>
+            <div className={styles.titles}>
+              <h2>Project Title</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Et.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et.
+              </p>
+            </div>
+            <ul>
+              <li>Figma</li>
+              <li>Figma</li>
+            </ul>
+            <button className={styles.readMore}>Read More</button>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }
