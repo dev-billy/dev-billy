da0b4704e0363ee11e411a50cee377e42321b948
Billy <billypatrick767@gmail.com>
2021-08-17 15:56:21 +0300

    added room project

---
+++ src/data/markdown/roomHomepage.md
@@ -0,0 +1,7 @@
+This project for a landing page of a hypothetical furniture store was a frontend challenge from [Frontend Mentor](https://frontendmentor.io).
+
+Frontend Mentor is a platform that helps you practice building websites by following a design and project brief.
+
+Creating this project has helped me refine my workflow and exposed me to work with more interesting layouts and challenge my responsive web design skills.While working on this project, I got to experience the beauty of flexbox in making layouts easy and using javascript in making the website more interactive.
+
+> The cool illustration from [undraw](https://undraw.co)
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,8 @@
 import devBillySite from "./images/dev-billy.png";
 import designSystem from "./images/design-system.png";
 import bookmark from "./images/bookmark.svg";
+import room from "./images/room-homepage.png";
+import thoughts from "./images/thoughts.svg";
 let projectList = [
   {
     id: 1,
@@ -25,6 +27,24 @@ let projectList = [
     demoLink: "https://design-system-liart.vercel.app/",
     sourceLink: "https://github.com/dev-billy/design-system",
     outroIllustrations: [bookmark],
+  },
+  {
+    id: 3,
+    title: "Room Homepage",
+    shortDescription:
+      "Landing page for a hypothetical furniture store, Built to challenge my css layout skills",
+    techStack: [
+      "HTML5",
+      "SCSS",
+      "JavaScript",
+      "Responsive Web Design",
+      "Flexbox",
+    ],
+    story: "roomHomepage.md",
+    image: room,
+    demoLink: "https://room-homepage-dev-billy.vercel.app/",
+    sourceLink: "https://github.com/dev-billy/room-homepage",
+    outroIllustrations: [thoughts],
   },
 ];
 
